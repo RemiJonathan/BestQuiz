8f900cfb39ba1b6ebce27ee8ce5c598df940aa6c
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-09-24 19:34:05 -0400

    Added Back Button

--- app/src/main/java/com/example/bestquiz/MainActivity.java
+++ app/src/main/java/com/example/bestquiz/MainActivity.java
@@ -14,17 +14,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button trueButton;
     private Button falseButton;
     private ImageButton nextButton;
+    private ImageButton backButton;
     private int currentQuestionIndex = 0;
 
     private Question[] questionBank = new Question[]{
-            new Question(R.string.Anime_Kanade,false),
-            new Question(R.string.Anime_Megumin,false),
+            new Question(R.string.Anime_Kanade, false),
+            new Question(R.string.Anime_Megumin, false),
             new Question(R.string.Anime_Futabu, false),
             new Question(R.string.Anime_Elizabeth, true),
             new Question(R.string.Anime_Konosuba, true),
             new Question(R.string.Anime_Naruto, true),
             new Question(R.string.Anime_Miku, false),
             new Question(R.string.Anime_Alphonse, false),
+            new Question(R.string.Anime_Ash, true),
+            new Question(R.string.Anime_Bleach, true),
+            new Question(R.string.Anime_DXD, true),
+            new Question(R.string.Anime_Felix, true),
+            new Question(R.string.Anime_Kenshin, true)
 
     };
 
@@ -37,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         trueButton = findViewById(R.id.trueButton);
         falseButton = findViewById(R.id.falseButton);
         nextButton = findViewById(R.id.next_button);
+        backButton = findViewById(R.id.back_button);
 
 //        Question question = new Question(R.string.Anime_Kenshin,true);
 
@@ -44,13 +51,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         falseButton.setOnClickListener(this);
         trueButton.setOnClickListener(this);
         nextButton.setOnClickListener(this);
+        backButton.setOnClickListener(this);
 
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.trueButton:
                 //Toast.makeText(MainActivity.this, "True", Toast.LENGTH_SHORT).show();
                 checkAnswer(true);
@@ -61,13 +69,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.next_button:
                 //Toast.makeText(MainActivity.this, "Next", Toast.LENGTH_SHORT).show();
-                currentQuestionIndex =(currentQuestionIndex + 1)%questionBank.length;
+                currentQuestionIndex = (currentQuestionIndex + 1) % questionBank.length;
+                updateQuestion();
+                break;
+            case R.id.back_button:
+                //Toast.makeText(MainActivity.this, "Next", Toast.LENGTH_SHORT).show();
+                currentQuestionIndex = (currentQuestionIndex - 1) % questionBank.length;
                 updateQuestion();
                 break;
         }
     }
 
-    private void checkAnswer(boolean userChoseCorrect){
+    private void checkAnswer(boolean userChoseCorrect) {
         boolean answerIsTrue = questionBank[currentQuestionIndex].isAnswer();
 
         int toastMessageId = 0;
@@ -76,9 +89,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             toastMessageId = R.string.wrong_answer;
         }
-        Toast.makeText(MainActivity.this,toastMessageId,Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, toastMessageId, Toast.LENGTH_SHORT).show();
     }
-    public void updateQuestion(){
+
+    public void updateQuestion() {
         questionTV.setText(questionBank[currentQuestionIndex].getQuestionResId());
 
     }
